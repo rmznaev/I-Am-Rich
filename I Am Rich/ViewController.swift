313219579c32e1e650817ef0310e126998471f92
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-03-28 00:17:58 +0400

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ramazan Abdullayev on 11/26/18.
+//  Copyright © 2018 Ramazan Abdullayev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
